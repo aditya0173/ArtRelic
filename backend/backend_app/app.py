4a42624445aac8147db431ec03f807ea57cf4088
Adi-D-king <as4497119@gmail.com>
2025-04-09 11:00:40 +0530

    shema main updated

--- backend/backend_app/app.py
+++ backend/backend_app/app.py
@@ -1,5 +1,6 @@
 from flask import Flask, render_template, request, flash, jsonify  # Updated import
 from mongo_operation import insert_users,insert_location
+import datetime
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # Required for flash messages
@@ -12,10 +13,21 @@ def home():
 @app.route('/submit_user_info', methods=['POST'])
 def submit_user_info():
     data = request.form
-    name = data.get('name')
-    age = data.get('age')
-    print(f"Received name: {name}, age: {age}")
-    insert_users({'name': name, 'age': age})
+    user_id = data.get('user_id')
+    created_at = datetime.datetime.now()
+    user_name = data.get('user_name')
+    date_of_birth = data.get('date_of_birth')
+    email = data.get('email')
+    phone = data.get('phone')
+    region = data.get('region')
+    field_of_interst = data.get('field_of_interst')
+    field_of_research = data.get('field_of_research')
+    curently_working = data.get('curently_working')
+    graduaction_type = data.get('graduaction_type')
+    graduaction_year = data.get('graduaction_year')
+    graduaction_course = data.get('graduaction_course')
+    # print(f"Received name: {name}, age: {age}")
+    # insert_users({'name': name, 'age': age})
     return {"message":"User information submitted successfully!"}  # Use flash for alerts  # Render the template to display the alert
 
 @app.route('/submit_device_info', methods=['POST'])
